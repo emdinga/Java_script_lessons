a2e165e70930457f598fbef334ce8452b1667693
Emdinga <emdinga@gmail.com>
2024-04-09 12:40:33 -0700

    java script

---
+++ complete_jS/script.js
@@ -0,0 +1,10 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+let country = "South Africa";
+let population = "60 880 156"
+let continent = "Africa"
+
+console.log(country);
+console.log(continent);
+console.log(population);
